85a488b1f081827dffef853e03e9e9a127aa1275
florentabes <64966783+florentabes@users.noreply.github.com>
2023-07-18 10:42:32 +0200

    Update LocalSettings.php
    Ldap connection

--- wikibase/LocalSettings.php
+++ wikibase/LocalSettings.php
@@ -2,30 +2,51 @@
 
 // LOAD NUKE EXTENSION
 wfLoadExtension( 'Nuke' );
+wfLoadExtension( 'LDAPProvider' );
+wfLoadExtension( 'PluggableAuth' );
+wfLoadExtension( 'LDAPAuthentication2');
 
-// phpinfo();
+$LDAPProviderDomainConfigProvider = function() {
+    $config = [
+        'levant.abes.fr' => [
+            'connection' => [               
+                'server' => getenv('LDAP_SERVER'),
+                'port' => getenv('LDAP_PORT'),
+                'user' => getenv('LDAP_USER'),
+                'pass' => getenv('LDAP_PASS'),
+                'enctype' => 'clear',
+                'options' => [
+                    'LDAP_OPT_DEREF' => 1
+                ],
+                'basedn' => 'ou=personnels,ou=utilisateurs,dc=levant,dc=abes,dc=fr',
+                'userbasedn' => 'ou=personnels,ou=utilisateurs,dc=levant,dc=abes,dc=fr',
+                'groupbasedn' => 'ou=personnels,ou=utilisateurs,dc=levant,dc=abes,dc=fr',
+                'searchattribute' => 'samaccountname',
+                'usernameattribute' => 'samaccountname',
+                'realnameattribute' => 'cn',
+                'emailattribute' => 'mail',
+                'grouprequest' => 'MediaWiki\\Extension\\LDAPProvider\\UserGroupsRequest\\UserMemberOf::factory',
+                'presearchusernamemodifiers' => [
+                    'spacestounderscores',
+                    'lowercase'
+                ]
+            ]
+        ]
+    ];
+		return new \MediaWiki\Extension\LDAPProvider\DomainConfigProvider\InlinePHPArray($config);
+	};
 
-/*******************************/
-/* Enable Federated properties */
-/*******************************/
-#$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
 
-/*******************************/
-/* Enables ConfirmEdit Captcha */
-/*******************************/
-#wfLoadExtension( 'ConfirmEdit/QuestyCaptcha' );
-#$wgCaptchaQuestions = [
-#  'What animal' => 'dog',
-#];
+	// Si la connexion locale est également prise en charge, ces variables globales sont toujours nécessaires 
+$wgPluggableAuth_EnableLocalLogin  =  true ; 
+$LDAPAuthentication2AllowLocalLogin  =  true ; 
+$wgPluggableAuth_Class  =  "MediaWiki\\Extension\\LDAPAuthentication2\\PluggableAuth" ; 
+$wgPluggableAuth_ButtonLabel  =  "Connexion (PluggableAuth)" ;
 
-#$wgCaptchaTriggers['edit']          = true;
-#$wgCaptchaTriggers['create']        = true;
-#$wgCaptchaTriggers['createtalk']    = true;
-#$wgCaptchaTriggers['addurl']        = true;
-#$wgCaptchaTriggers['createaccount'] = true;
-#$wgCaptchaTriggers['badlogin']      = true;
 
-/*******************************/
-/* Disable UI error-reporting  */
-/*******************************/
-#ini_set( 'display_errors', 0 );
+$wgPluggableAuth_Config [ 'Log In (LDAP)' ]  =  [ 
+    'plugin'  =>  'LDAPAuthentication2' , 
+    'data'  =>  [ 
+        'domain'  =>  getenv('LDAP_DOMAIN')
+    ] 
+];
